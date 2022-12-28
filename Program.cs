a724aab53fd643cb5377604629d09ef4270b3750
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 17:17:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int alıs_fiyati, vergi_orani, kâr_orani,satis_fiyati;
+Console.WriteLine("Alış fiyatını yazınız : ");
+alıs_fiyati = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Vergi oranını yazınız : ");
+vergi_orani = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Kâr oranını yazınız : ");
+kâr_orani = Convert.ToInt32(Console.ReadLine());
+
+satis_fiyati=alıs_fiyati + (alıs_fiyati*vergi_orani)/100 + (alıs_fiyati*kâr_orani)/ 100;
+Console.WriteLine("Satış fiyatınız : " + satis_fiyati);
+
